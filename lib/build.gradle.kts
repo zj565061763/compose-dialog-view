f067ccf169670f47bfffcfc627e34547397e543c
zj565061763 <565061763@qq.com>
2023-02-27 17:22:59 +0800

    1.0.0-alpha11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-dialog-view"
-val libVersionName = "1.0.0-alpha10"
+val libVersionName = "1.0.0-alpha11"
 
 android {
     namespace = "com.sd.lib.compose.dialog"
