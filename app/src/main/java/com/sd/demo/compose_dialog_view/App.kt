b6e484eb9bdedbe0efd6f3711fb6d7c11317704d
zjun <565061763@qq.com>
2023-12-21 21:00:06 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_dialog_view/App.kt
+++ app/src/main/java/com/sd/demo/compose_dialog_view/App.kt
@@ -8,15 +8,24 @@ import androidx.compose.material3.Button
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.sd.demo.compose_dialog_view.ui.theme.AppTheme
+import com.sd.lib.compose.dialogview.DialogBehavior
 import com.sd.lib.compose.dialogview.DialogViewHook
+import com.sd.lib.vdialog.animator.scale.ScaleXYFactory
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // 拦截
         DialogViewHook.hook = { content ->
             AppTheme {
                 content()
             }
+        }
+
+        // 配置确认窗口行为
+        DialogBehavior.confirm {
+            this.animatorFactory = ScaleXYFactory()
         }
 
 //        hookConfirmViewButton()
