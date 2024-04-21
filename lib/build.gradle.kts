9f313f5ecc8fcdd39803fbd98134edb14f88c653
zjun <565061763@qq.com>
2024-04-21 22:26:40 +0800

    1.0.0-beta09

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-dialog-view"
-val libVersion = "1.0.0-beta08"
+val libVersion = "1.0.0-beta09"
 
 android {
     namespace = "com.sd.lib.compose.dialog"
